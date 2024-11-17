7655729c52a0dcb50ae9adaca845882c1eef8f55
gemada <gemadatam@gmail.com>
2024-11-17 22:52:30 +0300

    V3

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@
 import Head from 'next/head';
 import styles from './styles/Home.module.css';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import Alerts from './components/alerts';
+import { Badge } from "@/components/ui/badge"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
         <title>My Next.js App</title>
         <meta name="description" content="A simple Next.js app with header, footer, and hero section." />
@@ -13,27 +15,26 @@ export default function Home() {
 
       <header className={styles.header}>
         <h1 className={styles.logo}>MyApp</h1>
-
-      <SignedOut>
-        <SignInButton/>
-      </SignedOut>
-      <SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
           <UserButton />
-      </SignedIn>
-      <SignedIn>
-        
-      </SignedIn>
-
+        </SignedIn>
       </header>
 
-      <main className={styles.main}>
+      <main >
         <section className={styles.hero}>
           <h2>Welcome to MyApp</h2>
-          <p>Your journey starts here. Let’s explore together!</p>
+          <p>Your journey starts here. Lets explore together!</p>
+          <SignedIn>
+            <div>
+            <Alerts />
+            <Badge variant="destructive">Destructive</Badge>
+            </div>
+          </SignedIn>
         </section>
       </main>
-
-    
     </div>
   );
 }
